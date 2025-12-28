f114277406b6b5a4f6103687fdfdf05e19cca453
Alexeyts0Y <145836579+Alexeyts0Y@users.noreply.github.com>
2025-12-28 20:53:34 +0300

    Revert "Feature/#7"

--- backend/schema/request/perfect_weight_request_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Literal
-
-from schema.request.bmi_request_schema import BMIRequestSchema
-
-class PerfectWeightRequestSchema(BaseModel):
-    gender: Literal["male", "female"]
-    height: int = Field(ge=1, le=260)
